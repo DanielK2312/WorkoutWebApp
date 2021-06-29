c367b04a4f50d9472167cd4bf9d98716bf0bcc43
DanielK2312 <49923750+DanielK2312@users.noreply.github.com>
2021-06-29 06:37:27 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -20,20 +20,19 @@ class getUIElements {
     })
       .then(res => res.json())
       .then(data => {
-        console.log(data);
+        const div = document.getElementById('word-container')
         let output = '';
         data.forEach(element => {
-          // console.log(element.quote);
           output += `<h5>${element.quote}</h5>`;
         });
         // dynamically add quotes to html doc
         document.getElementById('quote').innerHTML = output;
-        document.getElementById('word-container').style.width = '300px';
-        document.getElementById('word-container').style.border = '15px solid';
-        document.getElementById('word-container').style.padding = '50px';
-        document.getElementById('word-container').style.margin = '20px';
-        document.getElementById('word-container').style.float = 'right';
-        document.getElementById('word-container').style.marginTop = '-300px';
+        div.style.width = '300px';
+        div.style.border = '15px solid';
+        div.style.padding = '50px';
+        div.style.margin = '20px';
+        div.style.float = 'right';
+        div.style.marginTop = '-300px';
       })
       .catch(err => {
         console.error(err);
@@ -68,6 +67,76 @@ class getUIElements {
   }
 
   // do the same to get other excercise types
+  // gets chest excercises from the url I created
+  getBack(url) {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data);
+        let output = '';
+        let counter = 1;
+        data.forEach(element => {
+          if (element.body_part === 'Back') {
+            // console.log(element.excercise);
+            output += `<h3>Excercise: ${counter}</h3>
+                     <li>${element.excercise}</li>
+                     <li>${element.sets}</li>
+                      `;
+            counter++;
+          }
+        });
+        document.getElementById('output').innerHTML = output;
+      })
+      .catch(err => console.log(err))
+  }
+
+  getLegs(url) {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data);
+        let output = '';
+        let counter = 1;
+        data.forEach(element => {
+          if (element.body_part === 'Legs') {
+            // console.log(element.excercise);
+            output += `<h3>Excercise: ${counter}</h3>
+                     <li>${element.excercise}</li>
+                     <li>${element.sets}</li>
+                      `;
+            counter++;
+          }
+        });
+        document.getElementById('output').innerHTML = output;
+      })
+      .catch(err => console.log(err))
+  }
+
+  // Error message for invalid input
+  showAlert = function (message) {
+    // create div
+    // const div = document.createElement('div');
+    const div = document.getElementById('error');
+    // add class
+    // div.className = `alert ${className}`;
+    // Add text
+    // div.appendChild(document.createTextNode(message));
+    div.innerHTML = `${message}`;
+    div.style.background = 'red';
+    // Get parent
+    const container = document.getElementById('container');
+
+    // get form
+    const form = document.getElementById('input');
+
+    // insert alert
+    container.insertBefore(div, form);
+
+    // Timeout after 3 sec
+    setTimeout(function () {
+      document.getElementById('error').remove();
+    }, 3000)
+  }
 }
 
 // instantiate getExcercises class
@@ -88,8 +157,18 @@ document.querySelector('.btn1').addEventListener('click', () => {
     elements.getRandomQuote();
     // clear text box after submitting
     elements.clearField();
+  } else if (userInput.toUpperCase() === 'back'.toUpperCase()) {
+    // get back excercises
+    elements.getBack('excercises.json');
+    elements.getRandomQuote();
+    elements.clearField();
+  } else if (userInput.toUpperCase() === 'legs'.toUpperCase()) {
+    // get leg excercises and quotes
+    elements.getLegs('excercises.json');
+    elements.getRandomQuote();
+    elements.clearField();
   } else {
-    // eventually want to change this, alerts are oogly
-    alert("Invalid Input");
+    // alert("Invalid Input");
+    elements.showAlert('Invalid Input, Please try again');
   }
-})
+});
